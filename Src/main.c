c1b66cba2493fec4abe44592de0af2b23c260195
JoCee <joceeporter@gmail.com>
2020-04-20 20:07:21 -0600

    i wish it was working

--- Src/main.c
+++ Src/main.c
@@ -21,55 +21,97 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
+static void MX_GPIO_Init(void);
 
-/* USER CODE END PFP */
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
+uint8_t disp1ay[38][8]={
+{0x3C,0x42,0x42,0x42,0x42,0x42,0x42,0x3C},//0
+{0x10,0x30,0x50,0x10,0x10,0x10,0x10,0x7c},//1
+{0x7E,0x2,0x2,0x7E,0x40,0x40,0x40,0x7E},//2
+{0x3E,0x2,0x2,0x3E,0x2,0x2,0x3E,0x0},//3
+{0x8,0x18,0x28,0x48,0xFE,0x8,0x8,0x8},//4
+{0x3C,0x20,0x20,0x3C,0x4,0x4,0x3C,0x0},//5
+{0x3C,0x20,0x20,0x3C,0x24,0x24,0x3C,0x0},//6
+{0x3E,0x22,0x4,0x8,0x8,0x8,0x8,0x8},//7
+{0x0,0x3E,0x22,0x22,0x3E,0x22,0x22,0x3E},//8
+{0x3E,0x22,0x22,0x3E,0x2,0x2,0x2,0x3E},//9
+{0x18,0x24,0x42,0x42,0x7E,0x42,0x42,0x42},//A
+{0x3C,0x22,0x22,0x3c,0x22,0x22,0x3C,0x0},//B
+{0x3C,0x40,0x40,0x40,0x40,0x40,0x40,0x3C},//C
+{0x7C,0x22,0x22,0x22,0x22,0x22,0x22,0x7C},//D
+{0x7C,0x40,0x40,0x7C,0x40,0x40,0x40,0x7C},//E
+{0x7C,0x40,0x40,0x7C,0x40,0x40,0x40,0x40},//F
+{0x3C,0x40,0x40,0x40,0x4c,0x44,0x44,0x3C},//G
+{0x44,0x44,0x44,0x7C,0x44,0x44,0x44,0x44},//H
+{0x7C,0x10,0x10,0x10,0x10,0x10,0x10,0x7C},//I
+{0x3C,0x8,0x8,0x8,0x8,0x8,0x48,0x30},//J
+{0x0,0x24,0x28,0x30,0x20,0x30,0x28,0x24},//K
+{0x40,0x40,0x40,0x40,0x40,0x40,0x40,0x7C},//L
+{0x81,0xC3,0xA5,0x99,0x81,0x81,0x81,0x81},//M
+{0x0,0x42,0x62,0x52,0x4A,0x46,0x42,0x0},//N
+{0x3C,0x42,0x42,0x42,0x42,0x42,0x42,0x3C},//O
+{0x3C,0x22,0x22,0x22,0x3C,0x20,0x20,0x20},//P
+{0x1C,0x22,0x22,0x22,0x22,0x26,0x22,0x1D},//Q
+{0x3C,0x22,0x22,0x22,0x3C,0x24,0x22,0x21},//R
+{0x0,0x1E,0x20,0x20,0x3E,0x2,0x2,0x3C},//S
+{0x0,0x3E,0x8,0x8,0x8,0x8,0x8,0x8},//T
+{0x42,0x42,0x42,0x42,0x42,0x42,0x22,0x1C},//U
+{0x42,0x42,0x42,0x42,0x42,0x42,0x24,0x18},//V
+{0x0,0x49,0x49,0x49,0x49,0x2A,0x1C,0x0},//W
+{0x0,0x41,0x22,0x14,0x8,0x14,0x22,0x41},//X
+{0x41,0x22,0x14,0x8,0x8,0x8,0x8,0x8},//Y
+{0x0,0x7F,0x2,0x4,0x8,0x10,0x20,0x7F},//Z
+};
 
-/* USER CODE END 0 */
-
-void transmit(char v){
-	while( !SPI1->SR >> 1 ){ //Checking transmit buffer empty flag. If 0, then it is not empty
+void write_byte (uint8_t byte)
+{
+	for (int i =0; i<8; i++)
+	{
+		HAL_GPIO_WritePin (GPIOA, GPIO_PIN_5, 0);  // pull the clock pin low
+		HAL_GPIO_WritePin (GPIOA, GPIO_PIN_7, byte&0x80);  // write the MSB bit to the data pin
+		byte = byte<<1;  // shift left
+		HAL_GPIO_WritePin (GPIOA, GPIO_PIN_5, 1);  // pull the clock pin HIGH
 	}
-	SPI1->DR = v;
 }
-void transmitWord(char* word){
-	int counter = 0;
-	char temp = word[counter];
-	
-	while( temp != '\0'){
-		transmit(temp);
-		temp = word[++counter];
+
+void write_max (uint8_t address, uint8_t data)
+{
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+	HAL_GPIO_WritePin (GPIOA, GPIO_PIN_6, 0);  // pull the CS pin LOW
+	write_byte (address);
+	write_byte (data); 
+	HAL_GPIO_WritePin (GPIOA, GPIO_PIN_6, 1);  // pull the CS pin HIGH
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+}
+
+
+
+// function for init
+
+void max_init(void)
+{
+ write_max(0x09, 0x00);       //  no decoding
+ write_max(0x0a, 0x03);       //  brightness intensity
+ write_max(0x0b, 0x07);       //  scan limit = 8 LEDs
+ write_max(0x0c, 0x01);       //  power down =0,normal mode = 1
+ write_max(0x0f, 0x00);       //  no test display
+}
+
+void write_string (char *str)
+{
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+	while (*str)
+	{
+		for(int i=1;i<9;i++)
+			   {
+			       write_max (i,disp1ay[(*str - 55)][i-1]);
+			   }
+		*str++;
+		HAL_Delay (500);
 	}
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
 }
 
 
@@ -80,67 +122,64 @@ void transmitWord(char* word){
 int main(void)
 {
 
-  HAL_Init();
+ HAL_Init();
 
   SystemClock_Config();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
 	
-	RCC->AHBENR |= 1<<19 | 1<<18 | 1<<17;	 //Enable clock IOPC & IOPB & IOPA
-	RCC->APB2ENR |= 1<<12; //Enable clock for SPI1
+	MX_GPIO_Init();
+	max_init ();
+	GPIO_InitTypeDef initStr = {GPIO_PIN_9,GPIO_MODE_OUTPUT_PP,GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
+	HAL_GPIO_Init(GPIOC, &initStr);
+	GPIO_InitTypeDef initStr3 = {GPIO_PIN_6| GPIO_PIN_7,GPIO_MODE_OUTPUT_PP,GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
+	HAL_GPIO_Init(GPIOC, &initStr3);
 	
 	//Setting up LEDs for testing + error checking.
-	GPIOC->MODER |= 0x55000;
-	GPIOC->OTYPER = 0x000;
-	GPIOC->OSPEEDR = 0x0;
-	GPIOC->PUPDR = 0x0;
+	GPIO_InitTypeDef initStr2 = {GPIO_PIN_8, GPIO_MODE_INPUT,GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
+	HAL_GPIO_Init(GPIOC, &initStr2);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
 	
-	//Baud Rate Do we need to configure this? Max is 10MHz, which is the clockrate of the Discovery Board
-	//SPI1->BR = HAL_RCC_GetHCLKFreq()/115200;
-	SPI1->CR1 |= 0x7<<5; //Baud Rate f/256.
 	
-	//CRC length to 16 bits TODO
-	SPI1->CR1 |= 1<<11; 
-	
-	//Set pins PB3-PB5 to alternative mode. Because we aren't doing an SPI receive, we aren't using the MISO
-	GPIOB->MODER |= 0x980;
-	GPIOA->MODER |= 2<<30; //NSS (Slave Selector) is PA15
-	
-	//TODO: do we need to do anything more to setup the altnerative functions?
-	
-	//select Simple Mode through BIDIMONE and BIDIOE
-	SPI1->CR1 |= 1<<15;		//TODO I don't think we need this. BIIMOE (bidirectional data mode enabled)
-	SPI1->CR1 |= 1<<14;		//Output Enabled a.k.a transmit-only mode
-	SPI1->CR1 |= 1<<2;		//Master Selection
-	//DMD expects 16-bit data
-	SPI1->CR2 |= 0xf << 8;	//Bits [8:11] = 1111
-
-
-	SPI1->CR1 |= 1<<6; //SPI Enable
-	GPIOC->ODR |= (1<<9);
 	while(1){
-		GPIOC->ODR &= ~(1<<6);	//Sanity check. Turning LED
-		GPIOC->ODR &= ~(1<<7);	//Sanity check. Turning LED
-		GPIOC->ODR &= ~(1<<8);	//Sanity check. Turning LED
-		HAL_Delay(1000);
-		
-		
-		GPIOC->ODR ^= (1<<9);
-		while (!SPI1->SR >> 7) //busy flag is on do nothing.
-		{
-			GPIOC->ODR |= (1<<6);	//Sanity check. Turning on a color
-		}
-		if(SPI1->SR >> 8){
-			GPIOC->ODR |= (1<<8);		//Frame error.
-		}
-		if(SPI1->SR >> 3){
-			GPIOC->ODR |= (1<<7);		//Underrun error.
-		}
-		
-		
-		char message[] = "Hi";
-		//while there is still data to send, and the transfer buffer has room, send the next letter.
-		transmitWord(message);
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+		write_string ("HI");
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+		HAL_Delay (1000);
 	}
 }
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+	static void MX_GPIO_Init(void)
+	{
+		GPIO_InitTypeDef GPIO_InitStruct = {0};
+		GPIO_InitTypeDef GPIO_InitStruct2 = {0};
+
+		/* GPIO Ports Clock Enable */
+		__HAL_RCC_GPIOD_CLK_ENABLE();
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+
+		/*Configure GPIO pin Output Level */
+		HAL_GPIO_WritePin(GPIOA, clock_Pin|cs_Pin|data_Pin, GPIO_PIN_RESET);
+
+		/*Configure GPIO pins : clock_Pin cs_Pin data_Pin */
+		GPIO_InitStruct2.Pin = data_Pin;
+		GPIO_InitStruct2.Mode = GPIO_MODE_AF_OD;
+		GPIO_InitStruct2.Pull = GPIO_PULLUP;
+		GPIO_InitStruct2.Speed = GPIO_SPEED_FREQ_LOW;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct2);
+		
+		GPIO_InitStruct.Pin = clock_Pin|cs_Pin;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+		GPIOB->AFR[0] = 0;
+
+	}
 
 /**
   * @brief System Clock Configuration
@@ -174,6 +213,7 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 }
+
 
 /* USER CODE BEGIN 4 */
 
